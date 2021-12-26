13e6b448e8a3ce5268c25c217f551a8957848e0a
Michał <sukowskimichal@gmail.com>
2021-12-26 10:05:10 +0100

    cors middleware

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const data = require("./data.js");
+
+//middleware
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Homepage");
+});
+
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
 });
 
 app.listen(5000, () => {
