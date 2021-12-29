d4fb346a7bb60972283ab4dfd8d5853da72670d6
Michał <sukowskimichal@gmail.com>
2021-12-29 21:53:21 +0100

    users in database

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -5,8 +5,13 @@ const data = require("../data");
 const userRouter = express.Router();
 
 userRouter.get("/seed", async (req, res) => {
-  const createdUsers = await User.insertMany(data.users);
-  res.send({ createdUsers });
+  try {
+    const createdUsers = await User.insertMany(data.users);
+    res.send({ createdUsers });
+  } catch (err) {
+    console.log(err);
+    res.send({ error: err.errmsg });
+  }
 });
 
 module.exports = userRouter;
