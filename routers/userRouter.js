149e74be0402a00d88cb69ef37bba7bd905c7950
Michał <sukowskimichal@gmail.com>
2022-01-04 23:39:43 +0100

    register API

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -22,17 +22,28 @@ userRouter.post("/signin", async (req, res) => {
     //if password matches
     console.log("user is true");
     if (bcrypt.compareSync(req.body.password, user.password)) {
-      res.send({
+      res.status(200).send({
         _id: user._id,
         name: user.name,
         email: user.email,
         isAdmin: user.isAdmin,
         token: generateToken(user),
       });
+      return;
     }
-  } else {
-    res.status(401).send({ error: "Invalid email or password" });
   }
+  res.status(401).send({ error: "Invalid email or password" });
+});
+
+userRouter.post("/register", async (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: bcrypt.hashSync(req.body.password, 8),
+  });
+
+  await user.save();
+  res.send({ createdUser: user });
 });
 
 module.exports = userRouter;
