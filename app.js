dd8e6bcdaff57dbffbcd5848c66a11a29520b312
Michał <sukowskimichal@gmail.com>
2022-01-15 12:19:06 +0100

    paypal api

--- app.js
+++ app.js
@@ -30,6 +30,9 @@ app.use(bodyParser.json());
 app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
+app.get("/api/config/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
+});
 
 app.get("/", (req, res) => {
   res.send("Homepage");
